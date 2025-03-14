c608755e51ce743a985b92668cc57dbd3b02c756
Poorvi Biradar <167499602+poorviBiradar17@users.noreply.github.com>
2025-03-14 16:26:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, PES\n";
+  cout << "Hello, SEC G \n";
+  cout << "I have changed  \n";
   return 0;
 }
